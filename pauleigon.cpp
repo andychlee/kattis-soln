9f568afa03f5136e71a88fead2c48d7bef69a1f1
andychlee <ch28lee@uwaterloo.ca>
2017-10-08 21:07:36 -0400

    pauleigon

---
+++ pauleigon.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int N, P, Q;
+    cin >> N >> P >> Q;
+    if (((P + Q) / N) % 2 == 0) {
+        cout << "paul" << endl;
+    } else {
+        cout << "opponent" << endl;
+    }
+}
